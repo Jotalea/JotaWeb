fa786d043d779d686a28402a83262f6163c968db
Jotalea <67925603+Jotalea@users.noreply.github.com>
2024-07-10 17:13:08 -0300

    Update view-notbrowse.py

--- view-notbrowse.py
+++ view-notbrowse.py
@@ -1,3 +1,4 @@
+# Import modules
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
@@ -5,44 +6,41 @@ from PyQt5.QtCore import *
 from PyQt5.QtWebEngineWidgets import *
 from PyQt5.QtWidgets import QWidget
 
+START_URL = "http://google.com" # Example URL
+
+# Main class
 class JotaleaWebView(QMainWindow):
+    # The function that runs when running the class
     def __init__(self, *args, **kwargs):
         super(JotaleaWebView, self).__init__(*args, **kwargs)
 
+        # Create a Qt window and name it
         self.window = QWidget()
         self.window.setWindowTitle("Jotalea WebView")
 
+        # Create a Qt layout
         self.layout = QVBoxLayout()
-        self.horizontal = QHBoxLayout()
 
-        self.bar_url = QTextEdit()
-        self.bar_url.setMaximumHeight(30)
-
-        self.btn_go = QPushButton("Go")
-        self.btn_go.setMinimumHeight(30)
-
-        self.btn_back = QPushButton("<")
-        self.btn_back.setMinimumHeight(30)
-
-        self.btn_forward = QPushButton(">")
-        self.btn_forward.setMinimumHeight(30)
-
-        self.horizontal.addWidget(self.bar_url)
-        self.horizontal.addWidget(self.btn_go)
-        self.horizontal.addWidget(self.btn_back)
-        self.horizontal.addWidget(self.btn_forward)
-
+        # Create a webview instance
         self.browser = QWebEngineView()
 
-        #self.layout.addLayout(self.horizontal)
+        # Add the webview to the layout
         self.layout.addWidget(self.browser)
 
-        self.browser.setUrl(QUrl("http://google.com"))
+        # Set the webview's URL to the assigned URL
+        self.browser.setUrl(QUrl(START_URL))
 
+        # Apply the layout to the window
         self.window.setLayout(self.layout)
+
+        # Show the window to the user
         self.window.show()
 
-
+# Initialize the Qt Application
 app = QApplication([])
+
+# Invoke the main window
 window = JotaleaWebView()
+
+# Run
 app.exec_()
